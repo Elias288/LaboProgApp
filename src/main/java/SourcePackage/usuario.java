d85ce6cc046a0a91e201aa1fb5bebb73d7589fd1
francheskito <franccesco.giordano11@gmail.com>
2020-08-26 21:06:59 -0300

    primeras clases usuario docente y alumno

---
+++ src/main/java/SourcePackage/usuario.java
@@ -0,0 +1,20 @@
+package SourcePackage;
+
+
+public class usuario {
+    //aca van los atributos xD
+    String nickname;
+    String nombre;
+    String apellido;
+    String correo;
+    String fechaparadesarmar;
+}
+ class alumno extends usuario {
+    //aca van los atributos xD
+
+}
+
+ class docente extends usuario {
+    //aca van los atributos xD
+   
+}
