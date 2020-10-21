70383b73e4b37bd15b92aaca5283a45918ad68a0
Elias288 <69608624+Elias288@users.noreply.github.com>
2020-10-21 18:40:55 -0300

    Update VerificadorLogin.java

--- LaboProgAppWA/src/main/java/com/mycompany/laboprogappwa/VerificadorLogin.java
+++ LaboProgAppWA/src/main/java/com/mycompany/laboprogappwa/VerificadorLogin.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.laboprogappwa;
 
 import java.io.IOException;
@@ -13,10 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Elias
- */
 @WebServlet(name = "Verificador", urlPatterns = {"/Verificador"})
 public class VerificadorLogin extends HttpServlet {
 
