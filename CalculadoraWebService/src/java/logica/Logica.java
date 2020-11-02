a8e3d4a900207fb18ce7d9efcc005cbe5b9452a5
Elias Bianchi <elias.bianchi@utec.edu.uy>
2020-11-02 16:14:22 -0300

    ejemplo calculadora

---
+++ CalculadoraWebService/src/java/logica/Logica.java
@@ -0,0 +1,16 @@
+package logica;
+
+public class Logica {
+
+    public Logica(){
+    }
+
+    public int sumar(int i, int j){
+        return (i+j);
+    }
+
+    public int restar(int i, int j){
+        return (i-j);
+    }
+
+}
---
+++ CalculadoraWebService/src/java/servidor/Main.java
@@ -0,0 +1,9 @@
+
+package servidor;
+
+public class Main {
+    public static void main(String[] args) {
+        Publicador p = new Publicador();
+        p.publicar();
+    }
+}
